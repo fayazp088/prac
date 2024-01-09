9257f8743a79da8a5d1a26e92961f9d8f6ff654e
fayaz <fayazp088@gmail.com>
2024-01-10 00:02:06 +0530

    started with cicd finally

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type PingHandler struct {
+	Message string `json:"message"`
+	Status  int    `json:-`
+}
+
+func pingHandler(w http.ResponseWriter, r *http.Request) {
+	ping := &PingHandler{
+		Message: `Pong`,
+		Status:  http.StatusOK,
+	}
+
+	pingBytes, err := json.Marshal(ping)
+
+	if err != nil {
+		fmt.Errorf("Internal Server Error....", err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(ping.Status)
+
+	w.Write(pingBytes)
+}
+
+func main() {
+
+	http.HandleFunc("/ping", pingHandler)
+	http.ListenAndServe(":8080", nil)
+}
